7b0c25bf5a4108732cee5cf0603330b8c1f81922
anitasaunier <asaunier@udesa.edu.ar>
2021-04-10 11:38:00 -0300

    cambios rutas

---
+++ programacion2_proyecto/routes/producto.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/productoController')
+/* GET users listing. */
+router.get();
+router.get();
+
+router.get();
+module.exports = router;
