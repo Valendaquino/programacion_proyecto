f624aaa88c557bcfd6c0dc553a16bae7a6f9ac1d
paulinaruiz <pruiz@udesa.edu.ar>
2021-05-30 17:29:13 -0300

    trabajamos en los controladores

--- programacion2_proyecto/controllers/productoControllers.js
+++ programacion2_proyecto/controllers/productoControllers.js
@@ -13,27 +13,7 @@ let controller = {
        },
     
     agregarproducto: function(req,res){
-        return res.render('product-add')
-    },
-    almacenar: function(req,res){
-        type.findAll()
-        .then(function (type_product){
-            return res.render('product-add', {type_product})
-        })
-        .catch(err => console.log(err))
-        let product = {
-            name_: req.body.name,
-            publish_date: req.body.date,
-            description: req.body.description,
-            url_image: req.body.length.img,
-            //user_id:req.body.user
-            type_id: req.body.type_id
-            }
-
-        db.Product.create(product)
-    
-            .then(() => res.redirect('/')) //la ruta esta bien?
-            .catch(err => console.log(err))  
+        res.render('product-add')
     },
     //borramos un this.producto(no sabiamos xa que servia)
     producto: function(req,res){
@@ -43,14 +23,16 @@ let controller = {
         .then((resultados)=> res.render(`product`,{resultados}))
         .catch((err)=>`Error:${err}`)
         
-               
-            
-        
-        
         
     },
     search: function(req,res){
-        res.render('search-results', {'producto' :producto})
+        let serchData= req.query.search.
+        product.findAll({
+            where:[{name_: {[op.like]:`${serchData}`}}],
+         // que onda esto ?   where:[{type_id: {[op.like]:`${serchData}`}}] 
+        })
+        .then((resultados)=> res.render(`search-results`,{resultados}))
+        .catch((err)=>`Error:${err}`)
     }
 }
 module.exports = controller
