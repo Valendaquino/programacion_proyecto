41e4d44531444699674f5bcf564f78419a4a7192
paulinaruiz <pruiz@udesa.edu.ar>
2021-06-03 18:23:21 -0300

    base actualizada

--- programacion2_proyecto/beauty_world.sql
+++ programacion2_proyecto/beauty_world.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE beauty_world2 CHARACTER SET utf8mb4;
 
-
 USE beauty_world2;
 
 CREATE TABLE users(
@@ -8,10 +7,13 @@ CREATE TABLE users(
     name_users VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL,
-    birth_date DATE NOT NULL,
+    birth_date DATE ,
     email VARCHAR (50) NOT NULL,
     password_ VARCHAR(500) NOT NULL,
-    confirm_password VARCHAR(500)  NOT NULL
+    confirm_password VARCHAR(500)  NOT NULL,
+    profile_photo VARCHAR(250),
+	created_at DATE ,
+    updated_at DATE
 );
 
 CREATE TABLE types_products(
@@ -21,13 +23,14 @@ CREATE TABLE types_products(
 CREATE TABLE products(
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name_ VARCHAR(50) NOT NULL,
-	publish_date DATE NOT NULL,
+	publish_date DATE,
     description VARCHAR (500) NOT NULL,
     url_image VARCHAR(250),
     user_id INT UNSIGNED,
     FOREIGN KEY (user_id) REFERENCES users(id),
     type_id INT UNSIGNED,
-	FOREIGN KEY (type_id) REFERENCES types_products(id)
+	FOREIGN KEY (type_id) REFERENCES types_products(id),
+    updated_at DATE
 );
 
 
@@ -35,7 +38,7 @@ CREATE TABLE products(
 CREATE TABLE comments(
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     text_ VARCHAR (500),
-    comment_date DATE NOT NULL,
+    comment_date DATE,
 	user_id INT UNSIGNED,
     product_id INT UNSIGNED,
 	FOREIGN KEY (user_id) REFERENCES users(id),
@@ -43,13 +46,13 @@ CREATE TABLE comments(
 );
 
 
-INSERT INTO users VALUES (default, 'Martina', 'Stoessel','tinistoessel_', '1997/03/29',  'tinistoessel@gmail.com', 'hola123','hola123');
-INSERT INTO users VALUES (default, 'Carla', 'Roca','carla.rocaa', '1999/04/09',  'carlita.roca@hotmail.com', 'abc234','abc234');
-INSERT INTO users VALUES (default, 'Valeria', 'Gonzales', 'valegonzales__', '1987/02/15',  'valuncha_loca@gmail.com', '15febrero','15febrero');
-INSERT INTO users VALUES (default, 'Sofia', 'Rimolo', 'sofa_rimolo', '2001/09/13',  'sofitarimolo@yahoo.com', 'sofa789','sofa789');
-INSERT INTO users VALUES (default, 'Santiago', 'Salado', 'tot.salado', '1998/06/11',  'totosalado@yahoo.com', 'totito145','totito145');
-INSERT INTO users VALUES (default, 'Rocio', 'fagalde', 'rocifagalde', '2003/02/02',  'roci_fag@yahoo.com', 'holamanola','holamanola');
-INSERT INTO users VALUES (default, 'Jose', 'Garcia', 'josecito_makeup', '2001/09/13',  'josecito123@yahoo.com', 'helloworld2','helloworld2');
+INSERT INTO users VALUES (default, 'Martina', 'Stoessel','tinistoessel_', '1997/03/29',  'tinistoessel@gmail.com', 'hola123','hola123','milkBlush.jpg', '2021/02/12', '2021/02/12');
+INSERT INTO users VALUES (default, 'Carla', 'Roca','carla.rocaa', '1999/04/09',  'carlita.roca@hotmail.com', 'abc234','abc234', 'milkBlush.jpg','2021/02/12', '2021/02/12');
+INSERT INTO users VALUES (default, 'Valeria', 'Gonzales', 'valegonzales__', '1987/02/15',  'valuncha_loca@gmail.com', '15febrero','15febrero','milkBlush.jpg', '2021/02/12', '2021/02/12');
+INSERT INTO users VALUES (default, 'Sofia', 'Rimolo', 'sofa_rimolo', '2001/09/13',  'sofitarimolo@yahoo.com', 'sofa789','sofa789', 'milkBlush.jpg','2021/02/12', '2021/02/12');
+INSERT INTO users VALUES (default, 'Santiago', 'Salado', 'tot.salado', '1998/06/11',  'totosalado@yahoo.com', 'totito145','totito145', 'milkBlush.jpg', '2021/02/12', '2021/02/12');
+INSERT INTO users VALUES (default, 'Rocio', 'fagalde', 'rocifagalde', '2003/02/02',  'roci_fag@yahoo.com', 'holamanola','holamanola', 'milkBlush.jpg','2021/02/12' ,'2021/02/12');
+INSERT INTO users VALUES (default, 'Jose', 'Garcia', 'josecito_makeup', '2001/09/13',  'josecito123@yahoo.com', 'helloworld2','helloworld2','milkBlush.jpg', '2021/02/12', '2021/02/12');
 
 INSERT INTO types_products VALUES (default, 'Mascaras y Delineadores');
 INSERT INTO types_products VALUES (default, 'Bronzers');
@@ -60,18 +63,18 @@ INSERT INTO types_products VALUES (default, 'Iluminadores y Rubores');
 INSERT INTO types_products VALUES (default, 'Hidratantes y Primers');
 INSERT INTO types_products VALUES (default, 'Pintalabios');
 
-INSERT INTO products VALUES (default, 'MILK LIP + CHEEK', '2021/02/12','Una barra de color hidratante multiusos para labios y mejillas.', 'milkBlush.jpg', 2, 6);
-INSERT INTO products VALUES (default, 'LANCOME GRANDIOSE', '2021/01/02','Lancôme desvela con la máscara de pestañas Grandiôse una nueva técnica de aplicación para crear, sin esfuerzo, una mirada con unas pestañas perfectamente desplegadas, voluminosas y homogéneas.', 'lancomeMascara.png', 6, 6);
-INSERT INTO products VALUES (default, 'NAKED RELOADED', '2020/09/08','De Urban Decay. Estos 12 tonos novedosos son de todo menos corrientes y no tienen nada de básico... Esta paleta abarca desde mates sedosos y suaves satinados hasta tonos con más brillo y purpurinas de colores. ', 'nakedReloaded.jpg', 5, 6);
-INSERT INTO products VALUES (default, 'GLOSS BOMB UNIVERSAL LIP', '2020/12/21','De Fenty Beauty. El iluminador de labios Gloss Bomb Universal Lip Luminizer ofrece un brillo explosivo.Los labios lucen instantáneamente más turgentes y suaves, con una formula no pegajosa.', 'glosBomb.jpg', 3, 8);
-INSERT INTO products VALUES (default, 'BEAUTY BLENDER', '2021/10/05','Una herramienta de maquillaje reutilizable que permite la aplicación uniforme y sin desperdiciar productos de belleza. BeautyBlender tiene una estructura que permite absorber pequeñas cantidades de agua cuando está mojado.' , 'beautyBlender.jpg', 7 , 4);
-INSERT INTO products VALUES (default,'BENE TINT', '2020/04/05','Tinta de bronceado natural, tiene un efecto “rellenador”, también hace que luzca más suave y mejor la textura en general de la piel.', 'benetint.jpg', 1, 2);
-INSERT INTO products VALUES (default,'RUBOR SUNSHINE MAC', '2021/11/09','Polvo leve iluminador con tonalidad rosada, ideal para lucir un aspecto bronceado diariamente', 'macpolvobrillo.jpg', 2, 6 );
-INSERT INTO products VALUES (default,'BRONZER BRUSH', '2020/03/04','Herramienta ideal para polvos compactos o rubores, la mejor calidad de cerdas seleccionadas eco-friendly y cruelty-free', 'brush.jpeg', 4, 2);
-INSERT INTO products VALUES (default, 'ESTEE LAUDER RUN', '2020/06/08', 'Base compacta de excelente cobertura para utilizar en cualquier lugar y en cualquier momento', 'baseCompacta.jpeg', 3, 3);
-INSERT INTO products VALUES (default, 'WAVES, YVES SAINT LAURENT', '2020/03/09', 'Delineador de alta duración a prueba de agua, para lucir una mirada más profunda y desafiante', 'eyeliner.jpg', 4, 1);
-INSERT INTO products VALUES (default, 'MAC LIPGLOSS', '2020/02/05', 'Labial rosado con terminación total GLOSS. Disponible en más de 4 colores.', 'lipgloss.jpg', 5, 8);
-INSERT INTO products VALUES (default, 'NAKED CHERRY', '2021/05/05', 'Paleta de 12 tonalidades, una buena elección si buscas una mezcla de colores para el uso diario, sin dejar de lado algunos tonos más jugados e intensos.','paletaCherry.jpeg', 6, 5);
+INSERT INTO products VALUES (default, 'MILK LIP + CHEEK', '2021/02/12','Una barra de color hidratante multiusos para labios y mejillas.', 'milkBlush.jpg', 2, 6, '2021/02/12');
+INSERT INTO products VALUES (default, 'LANCOME GRANDIOSE', '2021/01/02','Lancôme desvela con la máscara de pestañas Grandiôse una nueva técnica de aplicación para crear, sin esfuerzo, una mirada con unas pestañas perfectamente desplegadas, voluminosas y homogéneas.', 'lancomeMascara.png', 6, 6, '2021/02/12');
+INSERT INTO products VALUES (default, 'NAKED RELOADED', '2020/09/08','De Urban Decay. Estos 12 tonos novedosos son de todo menos corrientes y no tienen nada de básico... Esta paleta abarca desde mates sedosos y suaves satinados hasta tonos con más brillo y purpurinas de colores. ', 'nakedReloaded.jpg', 5, 6, '2021/02/12');
+INSERT INTO products VALUES (default, 'GLOSS BOMB UNIVERSAL LIP', '2020/12/21','De Fenty Beauty. El iluminador de labios Gloss Bomb Universal Lip Luminizer ofrece un brillo explosivo.Los labios lucen instantáneamente más turgentes y suaves, con una formula no pegajosa.', 'glosBomb.jpg', 3, 8, '2021/02/12');
+INSERT INTO products VALUES (default, 'BEAUTY BLENDER', '2021/10/05','Una herramienta de maquillaje reutilizable que permite la aplicación uniforme y sin desperdiciar productos de belleza. BeautyBlender tiene una estructura que permite absorber pequeñas cantidades de agua cuando está mojado.' , 'beautyBlender.jpg', 7 , 4, '2021/02/12');
+INSERT INTO products VALUES (default,'BENE TINT', '2020/04/05','Tinta de bronceado natural, tiene un efecto “rellenador”, también hace que luzca más suave y mejor la textura en general de la piel.', 'benetint.jpg', 1, 2, '2021/02/12');
+INSERT INTO products VALUES (default,'RUBOR SUNSHINE MAC', '2021/11/09','Polvo leve iluminador con tonalidad rosada, ideal para lucir un aspecto bronceado diariamente', 'macpolvobrillo.jpg', 2, 6 , '2021/02/12');
+INSERT INTO products VALUES (default,'BRONZER BRUSH', '2020/03/04','Herramienta ideal para polvos compactos o rubores, la mejor calidad de cerdas seleccionadas eco-friendly y cruelty-free', 'brush.jpeg', 4, 2, '2021/02/12');
+INSERT INTO products VALUES (default, 'ESTEE LAUDER RUN', '2020/06/08', 'Base compacta de excelente cobertura para utilizar en cualquier lugar y en cualquier momento', 'baseCompacta.jpeg', 3, 3,'2021/02/12');
+INSERT INTO products VALUES (default, 'WAVES, YVES SAINT LAURENT', '2020/03/09', 'Delineador de alta duración a prueba de agua, para lucir una mirada más profunda y desafiante', 'eyeliner.jpg', 4, 1, '2021/02/12');
+INSERT INTO products VALUES (default, 'MAC LIPGLOSS', '2020/02/05', 'Labial rosado con terminación total GLOSS. Disponible en más de 4 colores.', 'lipgloss.jpg', 5, 8, '2021/02/12');
+INSERT INTO products VALUES (default, 'NAKED CHERRY', '2021/05/05', 'Paleta de 12 tonalidades, una buena elección si buscas una mezcla de colores para el uso diario, sin dejar de lado algunos tonos más jugados e intensos.','paletaCherry.jpeg', 6, 5, '2021/02/12');
 
 
 
